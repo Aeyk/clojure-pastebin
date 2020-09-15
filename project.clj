67db8e85565d9f2e67ae9fcb28848b58b5978944
Malik Kennedy <mksybr@gmail.com>
2020-09-15 11:22:25 -0700

    Commit to merge historys

---
+++ project.clj
@@ -0,0 +1,16 @@
+(defproject ixio "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/java.jdbc "0.7.8"]
+                 [org.xerial/sqlite-jdbc "3.23.1"]
+                 [compojure "1.1.1"]
+                 [hiccup "1.0.0"]]
+  :plugins [[lein-ring "0.7.1"]]
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}}
+  :repl-options {:init-ns ixio.core}
+  :ring {:handler ixio.routes/app})
+  
