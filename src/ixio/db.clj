f793ef65da8697606b8cb00e5997f88ed2cdefd1
Malik Kennedy <mksybr@gmail.com>
2020-09-25 12:28:43 -0700

    Add ownership to pastes relatoinship to users migrations and db acts

--- src/ixio/db.clj
+++ src/ixio/db.clj
@@ -1,5 +1,5 @@
  (ns ixio.db
-  (:require [clojure.java.jdbc :refer :all])
+  (:require [clojure.java.jdbc :as jdbc])
   (:gen-class))
 
 
@@ -8,6 +8,46 @@
    :subprotocol "sqlite"
    :subname     "db/pastes_development.db"})
 
+(defn get-all-pastes []
+  (jdbc/query my-db ["SELECT * FROM pastes WHERE 1=1 AND private = False;"]))
+
+(defn get-paste-by-id [id]
+    (jdbc/query my-db [ "SELECT * FROM pastes WHERE 1=1 AND id = ? AND private = FALSE;" id ]))
+
+(defn get-last-paste []
+  (jdbc/query my-db "SELECT * FROM pastes WHERE ID =(SELECT MAX(ID)  AND private = FALSE FROM pastes);"))
+
+(defn current-user []
+  (if true ;; TODO: (logged-in?) (user-id) (anon-id)
+    1))
+
+(defn create-paste [req]
+   ;; req
+  (jdbc/insert! my-db :pastes
+    (merge {:private false
+            :userid (current-user)} req)))
+
+
+(defn get-all-users []
+  (jdbc/query my-db ["SELECT * FROM users;"]))
+
+(defn get-user-by-id [id]
+  (jdbc/query my-db [ "SELECT * FROM users WHERE id= ?" id ]))
+
+(defn get-user-by-username [username]
+  (jdbc/query my-db [ "SELECT * FROM users WHERE username= ?" username ]))
+
+
+(defn get-last-user []
+  (jdbc/query my-db "SELECT * FROM    users WHERE ID = (SELECT MAX(ID) FROM users);"))
+
+(defn create-user [req]
+   ;; req
+  (jdbc/insert! my-db :users req)) 
+
+
+
+
 ;;wish this worked
 #_(defn tables
   []
@@ -15,63 +55,12 @@
 
 ;; todo how to get table names / column names
 
-(defn create-db
-  "create db and table"
-  []
-  (try
-    (db-do-commands my-db
-      (create-table-ddl :pastes
-        [[:timestamp :datetime :default :current_timestamp ]
-         [:id "INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL"]
-         [:body "CLOB NOT NULL"]]))
-       (catch Exception e
-         (println (.getMessage e)))))
-
-(defn drop-pastes
-  []
-  (try
-    (db-do-commands my-db
-      (drop-table-ddl :pastes))
-       (catch Exception e
-         (println (.getMessage e)))))
-
-(defn get-all-pastes []
-  (query my-db ["SELECT * FROM pastes;"]))
-
-(defn get-paste-by-id [id]
-  (query my-db [ "SELECT * FROM pastes WHERE id= ?" id ]))
-
-(defn get-last-paste []
-  (query my-db "SELECT * FROM    pastes WHERE ID = (SELECT MAX(ID) FROM pastes);"))
-
-(defn create-paste [req]
-   ;; req
-  (insert! my-db :pastes {:body req})) ;; TODO how to sanitize
-
-
-
-(defn get-all-users []
-  (query my-db ["SELECT * FROM users;"]))
-
-(defn get-user-by-id [id]
-  (query my-db [ "SELECT * FROM users WHERE id= ?" id ]))
-
-(defn get-user-by-username [username]
-  (clojure.java.jdbc/query my-db [ "SELECT * FROM users WHERE username= ?" username ]))
-
-
-(defn get-last-user []
-  (query my-db "SELECT * FROM    users WHERE ID = (SELECT MAX(ID) FROM users);"))
-
-(defn create-user [req]
-   ;; req
-  (insert! my-db :users req)) 
-
-
-
 
 #_(create-db)
 #_(create-paste {:body "Hello, World"})
 #_(drop-pastes)
 
 #_(get-pastes-by-id 1)
+
+;; (create-paste {:body "First anon-owned post" :private false})
+;; (get-last-paste)
