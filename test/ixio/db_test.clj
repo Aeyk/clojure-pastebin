1fdf5941b88740ae23e39284a4ef422734937be1
Malik Kennedy <mksybr@gmail.com>
2020-09-29 13:10:29 -0700

    One test and some boiler plate

---
+++ test/ixio/db_test.clj
@@ -0,0 +1,7 @@
+(ns ixio.db-test
+  (:require
+   [clojure.test :refer :all]))
+
+(run-tests)
+;; => {:test 0, :pass 0, :fail 0, :error 0, :type :summary}
+
---
+++ test/ixio/views_test.clj
@@ -0,0 +1,22 @@
+(ns ixio.views-test
+  (:require
+   [ixio.views :refer :all]
+   [ixio.routes :refer :all]
+   [clojure.test :refer :all]))
+
+(defn get-request [resource web-app & params]
+   (app {:request-method :get :uri resource :params (first params)}))
+
+
+(def routes ["/pastes" "/paste" "/paste/:id"
+             "/user" ;;TODO should show current user settings OR redirect to /login
+             "/login" ;; TODO view          
+             ])
+
+(deftest every-get-route-has-ok-status
+  (is (every? #(= % 200)
+        (map #(:status (request % app)) routes))))
+
+(run-tests)
+;; => {:test 1, :pass 1, :fail 0, :error 0, :type :summary}
+
