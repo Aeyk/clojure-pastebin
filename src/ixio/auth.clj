ea1f81362ed34161dc8ac40d7680b32961f078ae
Malik Kennedy <mksybr@gmail.com>
2020-10-06 16:55:41 -0700

    Added signup form

--- src/ixio/auth.clj
+++ src/ixio/auth.clj
@@ -17,9 +17,19 @@
    [ring.middleware.session :refer [wrap-session]]
    [ring.middleware.params :refer [wrap-params]]
    [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+   [clojure.string :as str]
    [ring.adapter.jetty :refer [run-jetty]])
   (:import java.net.URI))
-   
+
+(defn- create-user
+  [{:keys [username password admin] :as user-data}]
+  (-> (dissoc user-data :admin)
+    (assoc :identity username
+      :password (creds/hash-bcrypt password)
+      :roles (into #{::user} (when admin [::admin])))))
+
+(create-user {:username "Doofy" "passwords_suck!" false}) 
+
 (def users
   (atom
     {"friend"
@@ -34,6 +44,23 @@
       :roles #{::admin}}}))
 
 (derive ::admin ::user)
+
+(defn- signup-form
+  [flash]
+  [:div {:class "row"}
+   [:div {:class "columns small-12"}
+    [:h3 "Sign up "
+     [:small "(Any user/pass combination will do, as you are creating a new account or profile.)"]]
+    [:div {:class "row"}
+     [:form {:method "POST" :action "signup" :class "columns small-4"}
+      [:div "Username" [:input {:type "text" :name "username" :required "required"}]]
+      [:div "Password" [:input {:type "password" :name "password" :required "required"}]]
+      [:div "Confirm" [:input {:type "password" :name "confirm" :required "required"}]]
+      [:div "Make you an admin? " [:input {:type "checkbox" :name "admin"}]]
+      [:div
+       [:input {:type "submit" :class "button" :value "Sign up"}]
+       [:span {:style "padding:0 0 0 10px;color:red;"} flash]]]]]])
+
 (def login-form
   [:div {:class "row"}
    [:div {:class "columns small-12"}
@@ -43,6 +70,7 @@
       [:div "Username" [:input {:type "text" :name "username"}]]
       [:div "Password" [:input {:type "password" :name "password"}]]
       [:div [:input {:type "submit" :class "button" :value "Login"}]]]]]])
+
 (defn resolve-uri
   [context uri]
   (let [context (if (instance? URI context) context (URI. context))]
@@ -59,9 +87,9 @@
 (defroutes routes
   (GET "/" req
     (h/html5
-      
       [:h2 "Interactive form authentication"]
       [:p "This app demonstrates typical username/password authentication, and a pinch of Friend's authorization capabilities."]
+      (signup-form (:flash req))
       [:h3 "Current Status " [:small "(this will change when you log in/out)"]]
       [:p (if-let [identity (friend/identity req)]
             (apply str "Logged in, with these roles: "
@@ -84,6 +112,16 @@
       [:p (e/link-to (context-uri req "logout") "Click here to log out") "."]))
   (GET "/login" req
     (h/html5 login-form))
+  (POST "/signup"
+    {{:keys [username password confirm] :as params}  :params :as req}
+    (if (and (not-any? str/blank? [username password confirm])
+          (= password confirm))
+      (let [user (create-user (select-keys params [:username :password :admin]))]
+        ;; HERE IS WHERE YOU'D PUSH THE USER INTO YOUR DATABASES if desired
+        (friend/merge-authentication
+          (resp/redirect (context-uri req username))
+          user))
+      (assoc (resp/redirect (str (:context req) "/")) :flash "passwords don't match!")))
   (GET "/logout" req
     (friend/logout* (resp/redirect (str (:context req) "/"))))
   (GET "/requires-authentication" req
